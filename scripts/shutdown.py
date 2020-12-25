881baf0a08ded7c5958dd3edcae95162010f21c1
lilarcor <9455380+lilarcor@users.noreply.github.com>
2020-12-25 22:13:50 +0800

    Update shutdown.py

--- scripts/shutdown.py
+++ scripts/shutdown.py
@@ -1,77 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
-sys.path.append('/storage/lib/')
+sys.path.append('/storage/lib')
 import RPi.GPIO as GPIO
 import os
 import time
 from multiprocessing import Process
 
 #initialize pins
-poweroffPin = 26
-resetPin = 21
-ejectPin = 20
+powerPin = 3 #pin 5
 
 #initialize GPIO settings
 def init():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(poweroffPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(resetPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(ejectPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(powerPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setwarnings(False)
 
+#waits for user to hold button up to 1 second before issuing poweroff command
 def poweroff():
-  oldState = True
-  while True:
-    GPIO.wait_for_edge(poweroffPin, GPIO.FALLING)
+    while True:
+        GPIO.wait_for_edge(powerPin, GPIO.FALLING)
+        os.system("shutdown -h now")
 
-    state = GPIO.input(poweroffPin)
-    if state != oldState and state == False:
-
-      # print 'poweroff'
-      os.system("shutdown -h now")
-      time.sleep(1)
-    
-
-def reset():
-  oldState = True
-  while True:
-    GPIO.wait_for_edge(resetPin, GPIO.FALLING)
-
-    state = GPIO.input(resetPin)
-    if state != oldState and state == False:
-
-      # print 'reset'
-      os.system("pkill retroarch")
-      time.sleep(1)
-    
-
-
-def eject():
-  oldState = True
-  while True:
-    GPIO.wait_for_edge(ejectPin, GPIO.FALLING)
-
-    state = GPIO.input(ejectPin)
-    if state != oldState and state == False:
-
-      # print 'eject' 
-      os.system("shutdown -r now")
-      time.sleep(1)
-    
 
 
 if __name__ == "__main__":
-  #initialize GPIO settings
-  init()
-  #create a multiprocessing.Process instance for each function to enable parallelism 
-  poweroffProcess = Process(target = poweroff)
-  poweroffProcess.start()
-  resetProcess = Process(target = reset)
-  resetProcess.start()
-  ejectProcess = Process(target = eject)
-  ejectProcess.start()
+    #initialize GPIO settings
+    init()
+    #create a multiprocessing.Process instance for each function to enable parallelism
+    powerProcess = Process(target = poweroff)
+    powerProcess.start()
 
-  poweroffProcess.join()
-  resetProcess.join()
-  ejectProcess.join()
-
-  GPIO.cleanup()
+    powerProcess.join()
+    GPIO.cleanup()
